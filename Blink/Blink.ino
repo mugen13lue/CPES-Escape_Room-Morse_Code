e56e259126560f0ddf9619869d1a118c5c721e3c
Zack Soucek <zacksoucek@gmail.com>
2019-02-24 14:01:25 -0800

    made function to be used for wahtever led we need, oh and now theres a segfault

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,6 +1,5 @@
 
 #include <TimedAction.h>
-
 #include <FastLED.h>
 
 // How many leds in your strip?
@@ -12,152 +11,82 @@
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
 #define DATA_PIN 3
-enum { DOT, SPACE, DASH, S_END};
-int arr = DOT;
   
-
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
+//_________________________________________________light-up fuctions____________________________________
 
-
-  
-
-
-
-void dot(int i){
- leds[1] = CRGB::Red;
-
-//if on turns off, if off turns on
-  leds[i] = CRGB::Red;
-
-  
+void dot(int i, int r, int g, int b){
+  leds[i] = CRGB(r,g,b);
 }
-
-
-void dash(int i){
-  leds[1] = CRGB::Blue;
-
-  leds[i] = CRGB::Red;
- 
-
+void dash(int i, int r, int g, int b){
+  leds[i] = CRGB(r,g,b);
 }
-
-
-void space(int i){
-  leds[1] = CRGB::Green;
+void space(int i, int r, int g, int b){
+  //do nothing
 }
-
-void sEnd(int i){
-  leds[1] = CRGB::Purple;
-  
+void sEnd(int i, int r, int g, int b){  
+  //do nothing
 }
+//________________________________________________________________creating the timedActions and their respective functions_______________________________________
 
-
-  TimedAction LED0 =  TimedAction(TIME_UNIT, led0);
-  TimedAction LED2 = TimedAction(TIME_UNIT, led2);
-
-  
-  void setup() { 
-       FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);  
-     
-       
-}
+TimedAction LED0 =  TimedAction(TIME_UNIT, led0); 
 int pos0 = 0;
 void led0(){
-  if( leds[0].r > 0 || leds[0].g > 0 || leds[0].b > 0){
-    leds[0] = CRGB::Black;
-    LED0.setInterval(TIME_UNIT);
-    }else{      
-    int arr[] = {0,0,0,1,1,1,0,0,0,3};
-    switch( arr[pos0]){
-      case 0:
-        dot(0);
-        LED0.setInterval(TIME_UNIT);
-        break;
-      case 1:
-        dash(0);
-        LED0.setInterval(3*TIME_UNIT);
-        break;
-      case 2: 
-        space(0);
-        LED0.setInterval(4*TIME_UNIT);
-        break;
-      case 3: 
-        sEnd(0);
-        LED0.setInterval(6*TIME_UNIT);
-        break;
-    }
-  pos0 = (pos0 +1) %10 ;  
-  }
+  int arr[] = {0,0,0,1,1,1,0,0,0,3};
+  if (arbitraryLedDo(0, 64, 0 ,0, LED0, arr[pos0]) )
+    pos0 = (pos0 +1) %10; 
 }
+
+TimedAction LED2 = TimedAction(TIME_UNIT, led2);
 int pos2 = 0;
 void led2(){
-  if( leds[2].r > 0 || leds[2].g > 0 || leds[2].b > 0){
-    leds[2] = CRGB::Black;
-    LED2.setInterval(TIME_UNIT);
-    }else{
-      
-    int arr[] = {1,0,1,0,2};
-    switch( arr[pos2]){
-      case 0:
-        dot(2);
-        LED2.setInterval(TIME_UNIT);
-        break;
-      case 1:
-        dash(2);
-        LED2.setInterval(3*TIME_UNIT);
-        break;
-      case 2: 
-        space(2);
-        LED2.setInterval(4*TIME_UNIT);
-        break;
-      case 3: 
-        sEnd(2);
-        LED2.setInterval(6*TIME_UNIT);
-        break;
-    }
-  pos2 = (pos2 +1) %5 ;
-      
+  int arr[] = {1,0,1,0,2};
+  if (arbitraryLedDo(2, 128, 0, 0, LED2, arr[pos2]) )
+    pos2 = (pos2 +1) % 5 ;
+}
   
+//____________________________________________________setup____________________________________
+void setup() { 
+    FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);       
+}
+//_________________________________________________the singular function to handle the led activations____________
+
+boolean arbitraryLedDo(int ledNum, int r, int g, int b, TimedAction action, int instruction){
+  if( leds[ledNum].r > 0 || leds[ledNum].g > 0 || leds[ledNum].b > 0){
+      leds[ledNum] = CRGB::Black;
+      action.setInterval(TIME_UNIT);
+      return false;
+  }else{        
+      switch(instruction){
+        case 0:
+          dot(ledNum, r, g, b);
+          action.setInterval(TIME_UNIT);
+          break;
+        case 1:
+          dash(ledNum, r, g, b);
+          action.setInterval(3*TIME_UNIT);
+          break;
+        case 2: 
+          space(ledNum, r, g, b);//technically useless
+          action.setInterval(4*TIME_UNIT);
+          break;
+        case 3: 
+          sEnd(ledNum, r, g, b);//technically usless
+          action.setInterval(6*TIME_UNIT);
+          break;
+    }
+    return true;
   }
 }
-
-
  
  
-
-void loop() {
-  
-//    switch(arr){
-//      case SPACE:
-//        space();
-//        break;
-//      case DOT:
-//        dot(0);
-//        break;
-//      case DASH:
-//        dash(0);
-//        break;
-//      case S_END:
-//        sEnd();
-//        break;
-//    }
-
-//    int[10] arr1 = {0, 0, 2, 1)
-    
+//____________________________________main loop____________________________
+void loop() {    
     LED0.check();
-
-    LED2.check();
-    
-  
-        
-        
-  
-  
-
-
-  FastLED.show();
+    LED2.check(); 
+    FastLED.show();
 }
   
   
